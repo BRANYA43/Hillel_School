62851bdfc25b8825af73b0b58baa4be6db333ab5
Timchenko Bogdan <tbd2504@gmail.com>
2022-12-17 16:44:04 +0200

    Refactor scrip

--- Python_Pro/homework_2/task_1.py
+++ Python_Pro/homework_2/task_1.py
@@ -3,4 +3,4 @@ table = [[34587, 'Learning Python, Mark Lutz', 4, 40.95],
          [77226, 'Head First Python, Paul Barry', 3, 32.95],
          [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]]
 
-print(list(map(lambda row: tuple(round(item + 10, 2) if type(item) is float and item < 100 else item for item in row), table)))
+print(list(map(lambda row: (row[0], round(row[2] * row[3] + (10 if row[2] * row[3] < 100 else 0), 2)), table)))
