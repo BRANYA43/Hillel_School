4e7320cdaca1e8c47ff4ee6e8977cf400bac7ca1
Timchenko Bogdan <tbd2504@gmail.com>
2022-12-21 00:05:11 +0200

    Create DigitalCounter

---
+++ Python_Pro/homework_4/task_1.py
@@ -0,0 +1,22 @@
+def print_message_reaching_limit(func):
+    def wrapper(self, *args, **kwargs):
+        if self.current >= self.end:
+            print('Current is reaching the limit')
+        else:
+            func(self, *args, **kwargs)
+    return wrapper
+
+
+class DigitalCounter:
+    def __init__(self, start=0, end=100, current=None):
+        self.start = start if type(start) is int else 0
+        self.end = end if type(end) is int else 100
+        self.current = start if current is None else current
+
+    @print_message_reaching_limit
+    def increase(self):
+        if self.current < self.end:
+            self.current += 1
+
+    def get_current_value(self) -> int:
+        return self.current
