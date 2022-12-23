80933961092ac6c73aeb3584d62fae9cb93b3292
Timchenko Bogdan <tbd2504@gmail.com>
2022-12-24 00:41:49 +0200

    Add utils.py

---
+++ Python_Pro/homework_6/utils.py
@@ -0,0 +1,6 @@
+def get_module_list_in_html_str(module_list: list) -> str:
+    html_string = ''
+    for i, module in enumerate(module_list):
+        module = module.split("==")
+        html_string += '<p>[{}] <i>{} {}<i>'.format(i + 1, module[0], module[1])
+    return html_string
